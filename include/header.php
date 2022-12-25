c8fd3b39f4ca65c8f2776fa4c16cb1ba5e4d2a41
mdarbaz98 <zainshaikh2468@gmail.com>
2022-12-25 19:48:44 +0530

    favicon added

--- include/header.php
+++ include/header.php
@@ -6,6 +6,7 @@
     <title>Web</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="images/Layer 1.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.2/font/bootstrap-icons.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
