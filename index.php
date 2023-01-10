ec340e24fad15b5d899db53291f84fc7a3dc4340
mdarbaz98 <zainshaikh2468@gmail.com>
2023-01-11 01:19:10 +0530

    third section redesign

--- index.php
+++ index.php
@@ -21,10 +21,10 @@ include('./admin/include/config.php')
     <section class="section-2">
         <div class="container">
             <div class="inner-div">
-                <h2>Human mind is very sensitive to partners it does business with. It is your interactions at
+                <!-- <h2>Human mind is very sensitive to partners it does business with. It is your interactions at
                     every stage keeps them tied and connected</h2>
                 <p class="p">At HOUSE OF EXPERIENCE we preach emotions and Innovation to bridge the gap between
-                    Brands and customers</p>
+                    Brands and customers</p> -->
                 <div class="works">
                     <div>
                         <span></span>
@@ -32,14 +32,14 @@ include('./admin/include/config.php')
                         <p class="p2">
 
                         </p>
-                        <p style="font-size:18px"; class="para">The interactions a customer has with a brand is now made up of multiple touchpoints, both offline and online. As a result, customers now have heightened expectations of the brand exposure they are receiving. They expect any advertising, marketing, and branding to match their unique needs and interests. Without this component, 
+                        <p style="font-size:18px" ; class="para">The interactions a customer has with a brand is now made up of multiple touchpoints, both offline and online. As a result, customers now have heightened expectations of the brand exposure they are receiving. They expect any advertising, marketing, and branding to match their unique needs and interests. Without this component,
                             brands may fail to get their attention and business.
-                            </p>
-
-                        
-                            <p  style="font-size:18px"; class="para";>At House of XP we believe in the power of storytelling to create an emotional connection between brands, their products and customers. Developing a successful brand story telling strategy can help in driving conversations and , ultimately revenue growth. That's where we take a holistic approach to our clients' marketing needs, from ideation and strategy uptill execution.
                         </p>
-                        <p  style="font-size:18px";  class="para">We also loop these experiential campaigns with performance-based metrics to extract the juice of each penny spend.</p>
+
+
+                        <p style="font-size:18px" ; class="para" ;>At House of XP we believe in the power of storytelling to create an emotional connection between brands, their products and customers. Developing a successful brand story telling strategy can help in driving conversations and , ultimately revenue growth. That's where we take a holistic approach to our clients' marketing needs, from ideation and strategy uptill execution.
+                        </p>
+                        <p style="font-size:18px" ; class="para">We also loop these experiential campaigns with performance-based metrics to extract the juice of each penny spend.</p>
                     </div>
                     <div class="right-div">
                         <div class="high">
@@ -72,27 +72,51 @@ include('./admin/include/config.php')
             </div>
         </div>
     </section>
-    <section class="section-3 container">
-        <div class="inner-div">
-            <h2>
-                Services we offer               
-            </h2>
-            <p class="mb-4">
-
-            </p>
-
-            <div class="high">
-            <ul>
-            <li class="d-flex align-items-center"><a href="creative.php">Content and Creatives</a>
-
-                <li class="d-flex align-items-center"><a href="video.php">Video Production</a>
-                </li>
-                <li class="d-flex align-items-center"><a href="image.php">Image Management</a></li>
-                <li class="d-flex align-items-center"><a href="events.php">Event and Production</a></li>
-                <li class="d-flex align-items-center"><a href="marketing.php">Marketing Analytics</a>
-                </li>
-            </ul>
-</div>
+    <section class="section-3 container py-5">
+        <div class="new_services_section row">
+            <div class="col-12">
+                <h2 class="mb-md-5">Services we offer</h2>
+            </div>
+            <div class="col-12 col-md-4">
+                <a class="box" href="creative.php">
+                    <img src="images/b-women.jpg" alt="">
+                    <div class="box_content">
+                        Content & Creatives
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-4">
+                <a class="box" href="video.php">
+                    <img src="images/blog4.jpeg" alt="">
+                    <div class="box_content">
+                    Video Production
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-4">
+                <a class="box" href="image.php">
+                    <img src="images/Brand 1.png" alt="">
+                    <div class="box_content">
+                    Image Management
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-4">
+                <a class="box" href="events.php">
+                    <img src="images/blog1.jpeg" alt="">
+                    <div class="box_content">
+                    Event and Production
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-4">
+                <a class="box" href="marketing.php">
+                    <img src="images/blog-img.jfif" alt="">
+                    <div class="box_content">
+                    Marketing Analytics
+                    </div>
+                </a>
+            </div>
         </div>
     </section>
     <section class="section-5">
@@ -146,44 +170,45 @@ include('./admin/include/config.php')
                         <button class="nav-link" id="Delivery-tab" data-bs-toggle="tab" data-bs-target="#Delivery" type="button" role="tab" aria-controls="Delivery" aria-selected="false"><span>3</span> Delivery</button>
                     </li>
                 </ul>
-                </div>
-                <div class="inner">
+                
+            </div>
+            <div class="inner">
                 <strong><a href="contact.php">Contact us</a> <i class="fa-solid fa-chevron-up"></i></strong>
             </div>
-            </div>
+        </div>
         </div>
     </section>
-   
+
     <section class="section-6 container">
         <div class="inner-div">
             <h2>Blogs</h2>
             <p class="p3"> </p>
             <div class="blog-list">
                 <?php
-                		$stmt_blog = $conn->prepare("SELECT * FROM `post` WHERE status=1");
-                        $stmt_blog->execute();
-                        while($blog_data = $stmt_blog->fetch(PDO::FETCH_ASSOC)){
-                            $img_id = $blog_data['img_id'];
+                $stmt_blog = $conn->prepare("SELECT * FROM `post` WHERE status=1");
+                $stmt_blog->execute();
+                while ($blog_data = $stmt_blog->fetch(PDO::FETCH_ASSOC)) {
+                    $img_id = $blog_data['img_id'];
 
-                            $blog_image = $conn->prepare("SELECT * FROM `images` WHERE id=?");
-                            $blog_image->execute([$img_id]);
-                            while($blog_img = $blog_image->fetch(PDO::FETCH_ASSOC)){
-                                $blog_img_path=$blog_img['path'];
-                            }
+                    $blog_image = $conn->prepare("SELECT * FROM `images` WHERE id=?");
+                    $blog_image->execute([$img_id]);
+                    while ($blog_img = $blog_image->fetch(PDO::FETCH_ASSOC)) {
+                        $blog_img_path = $blog_img['path'];
+                    }
 
                 ?>
-                <div class="card">
-                    <div class="card-head">
-                        <a href="blogpage1.php"> <img  src="./admin/<?php echo $blog_img_path ?>" alt=""></a>
+                    <div class="card">
+                        <div class="card-head">
+                            <a href="blogpage1.php"> <img src="./admin/<?php echo $blog_img_path ?>" alt=""></a>
+                        </div>
+                        <div class="card-body">
+                            <span><?php echo $blog_data['title'] ?></span>
+                            <p>
+                            </p>
+                            <a href="<?php echo $blog_data['slug'] ?>">Read more<i class="fa-solid ms-3 fa-chevron-up"></i></a>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <span><?php echo $blog_data['title'] ?></span>
-                        <p>
-                        </p>
-                        <a href="<?php echo $blog_data['slug'] ?>">Read more<i class="fa-solid ms-3 fa-chevron-up"></i></a>
-                    </div>
-                </div>
-            <?php } ?>            
+                <?php } ?>
                 <!-- <div class="card">
                     <div class="card-head">
                     <a href="blogpage2.php"> <img  src="./images/blog2.jpeg" alt=""></a>
